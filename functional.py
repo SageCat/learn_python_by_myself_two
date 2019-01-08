2f40098b8015119a4eb01eb2a47322db4dae4187
sage <1070652242@qq.com>
2019-01-08 15:51:59 +0800

    learn functional and closure

--- functional.py
+++ functional.py
@@ -2,8 +2,6 @@
 # -*- coding:utf-8 -*-
 # Author:Sage Guo
 
-from operator import itemgetter
-
 
 def func(x, y, f):
 	return f(x) + f(y)
@@ -92,45 +90,107 @@ def build(x, y):
 	return lambda: x * x + y * y
 
 
-print(abs)
-a = -10
-print(abs(-10))
-# 函数名也是变量，是指向函数的变量
-print(func(-3, 4, abs))
-print(func(-4, -5, positive))
-# map第一个参数是函数
-# 第二个参数是一个序列
-# map将第一个参数的函数作用到序列的每一个元素上并返回一个Iterator
-result = map(abs, [-1, 2, -90, -56.3, 67])
-print(type(result))  # map类型
-print(result)
-print(next(result))  # 具有迭代器Iterator的特性
-print(list(result))  # 将返回的Iterator转换为序列
+# print(abs)
+# a = -10
+# print(abs(-10))
+# # 函数名也是变量，是指向函数的变量
+# print(func(-3, 4, abs))
+# print(func(-4, -5, positive))
+# # map第一个参数是函数
+# # 第二个参数是一个序列
+# # map将第一个参数的函数作用到序列的每一个元素上并返回一个Iterator
+# result = map(abs, [-1, 2, -90, -56.3, 67])
+# print(type(result))  # map类型
+# print(result)
+# print(next(result))  # 具有迭代器Iterator的特性
+# print(list(result))  # 将返回的Iterator转换为序列
+#
+# result_two = map(str, (1, 2, 3, 4, 5, 6))
+# print(result_two)
+# print(next(result_two))
+# print(list(result_two))
+# print(list(filter(is_odd, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
 
-result_two = map(str, (1, 2, 3, 4, 5, 6))
-print(result_two)
-print(next(result_two))
-print(list(result_two))
-print(list(filter(is_odd, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+# f = open("/Users/sage/PycharmProjects/learn_python_02/primes.txt", 'w')
+# for n in primes():
+# 	if n < 10:
+# 		print(n)
+# 		f.write(str(n) + '\r\n')
+# 	else:
+# 		f.close()
+# 		break
+#
+# L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+# L2 = sorted(L, key=itemgetter(0))
+# print(L2)
+# f = lazy_sum(1, 2, 3, 4, 5)
+# print(f)
+# print(f())
+# f1, f2, f3 = count_two()
+# print(f1())
+# print(f2())
+# print(f3())
+#
+# print(build(1, 2)())
 
-f = open("/Users/sage/PycharmProjects/learn_python_02/primes.txt", 'w')
-for n in primes():
-	if n < 10:
-		print(n)
-		f.write(str(n) + '\r\n')
-	else:
-		f.close()
-		break
 
-L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
-L2 = sorted(L, key=itemgetter(0))
-print(L2)
-f = lazy_sum(1, 2, 3, 4, 5)
-print(f)
-print(f())
-f1, f2, f3 = count_two()
-print(f1())
-print(f2())
-print(f3())
+def outer():
+	x = 1
+	y = 2
 
-print(build(1, 2)())
+	# 函数的闭包，只有在inner中被调用的outer变量，才会被记录到封闭作用域中
+	def inner():
+		print(x)
+
+	return inner
+
+
+def outer_func(some_func):
+	def inner():
+		print('before some_func')
+		r = some_func()
+		return r + 1
+	return inner
+
+
+def foo():
+	return 2
+
+
+# 一般装饰器都写两层，返回一个句柄，为了防止不显示调用就被调用
+def wrapper(func):
+	def inner():
+		print('before func')
+		# func()
+		print('func name is ', func.__name__)
+		print('after func')
+	return inner
+
+
+# def wrapper(func):
+# 	print('before func')
+# 	# func()
+# 	print(func.__name__)
+# 	print('after func')
+# 	return func
+
+
+@wrapper
+def func1():
+	print('func 1------')
+
+
+# decorate = outer_func(foo)
+# decorate_two = outer_func(decorate)
+# print(decorate)
+# decorate()
+# print(decorate_two)
+# decorate_two()
+print('----------------')
+# decorate = wrapper(func1)
+# decorate_two = wrapper(decorate)
+# print(decorate)
+# decorate()
+# print(decorate_two)
+# decorate_two()
+print(func1())
