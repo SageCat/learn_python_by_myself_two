f9debc1511ead886a56b906de4b8bc8d0aa9a9bc
sage <1070652242@qq.com>
2019-01-09 21:54:43 +0800

    add learn copy module

--- learn_copy.py
+++ learn_copy.py
@@ -9,7 +9,7 @@ def get_memory_address(variable):
 
 
 def print_spilt():
-	print('-------------------')
+	print('--------------------------')
 
 
 age = [12, 34, 56, [1, 2, 3, 4, 5]]
@@ -25,8 +25,11 @@ age[3].append(300)
 print('age is ', age)
 print('age_copy is ', age_copy)
 
+# 深度copy，copy的值和被copy的值指向不同的引用，一个变量修改，不会导致另一个变量内容的改变，
+# 但是一个变量中的'子引用'的修改会导致另一个对象中的'子引用'发生修改，因为他们指向的事同一个引用
 print_spilt()
 age_copy_two = copy.copy(age)
+# 地址不一致，后三位不同
 print(get_memory_address(age))
 print(get_memory_address(age_copy_two))
 age.append(90)
@@ -36,8 +39,11 @@ age[3].append(100)
 print('original value is: ', age)
 print('shallow copy value is :', age_copy_two)
 
+# 深度copy，copy的值和被copy的值指向不同的引用，一个变量修改，不会导致另一个变量内容的改变
+# 子引用的修改也不会导致另一个对象子引用发生修改
 print_spilt()
 age_deep_copy = copy.deepcopy(age)
+# 地址不一致，后四位不同
 print(get_memory_address(age))
 print(get_memory_address(age_deep_copy))
 age.append(1000)
