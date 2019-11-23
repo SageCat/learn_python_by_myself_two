52ddad3f158910db42f0b0210b95248f5f9c74dd
SageCat <sage.guo@qq.com>
2019-11-23 12:26:19 +0800

    modify mysql

--- learn_mysql.py
+++ learn_mysql.py
@@ -4,11 +4,11 @@
 import mysql.connector
 
 if __name__ == '__main__':
-	conn = mysql.connector.connect(user='root', password='123456', database='sage_test')
+	conn = mysql.connector.connect(user='root', password='123456', database='fr_test')
 	cursor = conn.cursor()
 	cursor.execute('SHOW TABLES')
 	print(cursor.fetchall())
-	cursor.execute('SELECT * FROM cityCode')
+	cursor.execute('SELECT * FROM dim_date_en where year = 2001 and month_of_year = 1')
 	print(cursor.fetchall())
 	print(cursor.column_names)
 	print(cursor.with_rows)
