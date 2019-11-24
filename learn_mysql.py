0f9d6ef3eff54c51c5b5825aae7d57c96a37ead6
Sage <1070652242@qq.com>
2019-11-24 12:15:47 +0800

    add csv

--- learn_mysql.py
+++ learn_mysql.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 	cursor = conn.cursor()
 	cursor.execute('SHOW TABLES')
 	print(cursor.fetchall())
-	cursor.execute('SELECT * FROM dim_date_en where year = 2001 and month_of_year = 1')
+	cursor.execute("SELECT * FROM dim_date_en where year = 2001 and month_of_year = 1")
 	print(cursor.fetchall())
 	print(cursor.column_names)
 	print(cursor.with_rows)
--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@
 # Author:Sage Guo
 
 import requests
-from selenium import webdriver
 
 # 请求发送到百度，并获取源码
 response = requests.get("http://www.baidu.com")
 # 指定编码为 utf-8
 response.encoding = 'UTF-8'
 print(response.text)
+print('-------------------')
 print(response.status_code)
+print('-------------------')
+print(requests.head("http://www.baidu.com").headers)
+print('-------------------')
 # 根据URL抓取图片并保存
 response = requests.get('https://pic3.zhimg.com/50/v2-06ac19750e235cfe8b97600c5c3700a1_400x224.png')
 print(response.content)
@@ -18,8 +21,3 @@ print(response.content)
 with open('/Users/sage/Pictures/test.jpg', 'wb') as f:
 	f.write(response.content)
 	print(f.close())
-
-driver = webdriver.Chrome()
-driver.get('http://www.baidu.com')
-print(driver.page_source)
-
