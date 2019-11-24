f1d0ec9199e7191fc89bb5eaf8f6601b05ee7c68
Sage <1070652242@qq.com>
2019-11-24 12:31:32 +0800

    add csv

--- read_file.py
+++ read_file.py
@@ -2,6 +2,6 @@ import pathlib as path
 import pandas as pd
 
 # pd.readcsv(path.Path().joinpath('data', 'date_data.csv'))
-# pd.readcsv(path.Path().joinpath('data', 'date_data.csv'))
-pd.readcsv("./data/date_data.csv")
+pd.readcsv(path.Path().joinpath('data', 'date_data.csv'))
+# pd.readcsv("./data/date_data.csv")
 # print(path.Path().joinpath('data', 'date_data.csv'))
