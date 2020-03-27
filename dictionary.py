4ad0a8bfdfec11b7cd1d25bd7fc3adf10ff90287
Sage <1070652242@qq.com>
2020-03-28 03:12:09 +0800

    20200328

--- dictionary.py
+++ dictionary.py
@@ -2,7 +2,18 @@
 # -*- coding:utf-8 -*-
 # Author:Sage Guo
 
-fruit = {"orange": "yellow fruit", "apple": "very sweet fruit", "banana": "very soft fruit"}
+fruit = {"orange": "yellow fruit",
+         "apple": "very sweet fruit",
+         "banana": "very soft fruit"}
+
 print(fruit)
 print(fruit["orange"])
+
+# %%
+for key, value in fruit.items():
+    print(key, '---', value)
+# %%
+for key, value in fruit.items():
+    print(key, '---', value)
+
 
